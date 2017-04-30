8e66a31ab6d05aca299fefef3db35417b15f7cde
victor335882 <victor335882@livemail.tw>
2017-04-30 21:57:36 +0800

    Lecture 39 Reducer
    //combineReducers is a function that
    //contain a mapping of all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import BookReducer from './reducer_book'
 
+//combineReducers is a function that
+//contain a mapping of all reducers
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+ecport default function() {
+  return[
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'}
+  ]
+}
