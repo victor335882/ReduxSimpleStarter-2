2331c7c71795b864b21e0504518be82674ae5f33
victor335882 <victor335882@livemail.tw>
2017-05-01 11:37:58 +0800

    lecture 45 Create action
    Create action and connect to our reducer.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,10 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 
+//
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
+
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
@@ -25,4 +29,17 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything return from this function,mapDispatchToProps,
+//will end up as props on the BookList container
+//this.props.selectBook: selectBook
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, the result should be passed
+  //to all of our reducers through dispatch function
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+
+}
+
+//Promote BookList from a component to a container - it needs to know
+//about this new dispatch method, selectBook.
+//Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
