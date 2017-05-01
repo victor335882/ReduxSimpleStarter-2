c72e3698edb033a38be98d5889ec49f111552545
victor335882 <victor335882@livemail.tw>
2017-05-01 17:57:09 +0800

    Lecture 49.1 error of default value
    //Due to our default state is null in reducer_active_book.js
    //we need to add a statement to prevent the following error
    //bundle.js:22004 Uncaught TypeError: Cannot read property 'title' of
    null

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,9 +3,20 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    {
+      //Due to our default state is null in reducer_active_book.js
+      //we need to add a statement to prevent the following error
+      //bundle.js:22004 Uncaught TypeError: Cannot read property 'title' of null
+    }
+    if(!this.props.book) {
+      return <div>Select a book to get started</div>
+    }
     return(
-      <div>Book Detail!</div>
-    )
+      <div>
+        <h3>Detail for:</h3>
+        <div>{this.props.book.title}</div>
+      </div>
+    );
   }
 }
 
